39728d13d11eeb3b6eb945c21d1485a6aaf8d235
duuook <3022531349@qq.com>
2024-05-15 11:54:34 +0800

    add qianduan

--- Staff/Staff/settings.py
+++ Staff/Staff/settings.py
@@ -55,8 +55,8 @@ ROOT_URLCONF = 'Staff.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        # 'DIRS': [BASE_DIR / 'templates']
+        # ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Staff/Staff/urls.py
+++ Staff/Staff/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import app01.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', app01.views.index),
 ]
